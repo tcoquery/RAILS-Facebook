279486f00deb2b8202875056467f9f1e8c2683c8
tcoquery <tcoquery@gmail.com>
2022-05-27 12:16:53 +0200

    Add the possibility to sign in with facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1032858427334988", "b3340fe590387360384ea591dc8e3f2e"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
