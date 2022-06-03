347d4ffc72cfc3210fbf8e45acdac114a9d344c9
tcoquery <tcoquery@gmail.com>
2022-06-03 13:15:42 +0200

    Reworked the comment feature to work with turbo stream posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,21 @@
 class CommentsController < ApplicationController
-  
-    def create
-      @post = Post.find(params[:comment][:post_id])
-      @comment = @post.comments.build(comment_params)
-  
-      if @post.save
-        flash[:success] = "Great! Your post has been created!"
-        redirect_to home_index_path
+  def create
+    @post = Post.find(params[:comment][:post_id])
+    @comment = @post.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
       else
-        flash.now[:error] = "Rats! Fix your mistakes, please."
-        render :new, status: :unprocessable_entity
+        format.html { render :new, status: :unprocessable_entity }
       end
     end
-  
-  private
-  
-  def comment_params
-      params.require(:comment).permit(:post_id, :body, :user_id)
   end
-  
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :post_id, :body)
+  end
+
 end
